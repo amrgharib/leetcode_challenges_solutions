ef70021d9fb0b5c632a0fd566b769ec0f38e1177
Amr Ibrahim Gharib <69079038+amrgharib@users.noreply.github.com>
2020-08-27 21:21:26 -0600

    Update 436_find_right_interval.py

--- Python/436_find_right_interval.py
+++ Python/436_find_right_interval.py
@@ -1,25 +1,35 @@
-# Top solution
-from bisect import bisect_right
-
-
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        d = {}
+        r_int = []
+        start, mp = zip(*sorted((x, i) for i, (x, _) in enumerate(intervals)))
+        for x, y in intervals: 
+            i = bisect_left(start, y)
+            r_int.append(mp[i] if 0 <= i < len(intervals) else -1) # mapping to original index 
+        return r_int 
 
-        for i, (start, end) in enumerate(intervals):
-            if start not in d: d[start] = i
-
-        starts = list(sorted(d))
-
-        result = []
-
-        for start, end in intervals:
-            if end in d:
-                result.append(d[end])
-            elif end > starts[-1]:
-                result.append(-1)
-            else:
-                idx = bisect_right(starts, end)
-                result.append(d[starts[idx]])
-
-        return result
+# 
+# # Top solution
+# from bisect import bisect_right
+# 
+# 
+# class Solution:
+#     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+#     d = {}
+# 
+# for i, (start, end) in enumerate(intervals):
+#     if start not in d: d[start] = i
+# 
+# starts = list(sorted(d))
+# 
+# result = []
+# 
+# for start, end in intervals:
+#     if end in d:
+#     result.append(d[end])
+# elif end > starts[-1]:
+#     result.append(-1)
+# else:
+#     idx = bisect_right(starts, end)
+# result.append(d[starts[idx]])
+# 
+# return result
